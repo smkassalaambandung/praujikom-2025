7b8166454cf061327f38a16bd86c4f65d7c3bdc4
mas-olipiskandar <mas@olipiskandar.com>
2025-01-10 16:03:37 +0700

    Refactor authentication and event handling; replace ApiController and EventController with new API-specific implementations

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreEventRequest;
 use App\Http\Requests\UpdateEventRequest;
 use App\Models\Event;
