5c5959a4a79e29754f21e9f1a74267d1f77ea844
mas-olipiskandar <mas@olipiskandar.com>
2025-01-10 16:11:39 +0700

    Refactor EventController; remove request validation classes and streamline event creation, update, and deletion methods

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreEventRequest;
-use App\Http\Requests\UpdateEventRequest;
 use App\Models\Event;
+use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
@@ -28,9 +27,16 @@ class EventController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreEventRequest $request)
+    public function store(Request $request)
     {
-        //
+        $event = new Event();
+        $event->name = $request->name;
+        $event->description = $request->description;
+        $event->date = $request->date;
+        $event->location = $request->location;
+        $event->user_id = $request->user_id;
+        $event->save();
+        return response()->json(['message' => 'Event created successfully', 'event' => $event], 201);
     }
 
     /**
@@ -52,16 +58,27 @@ class EventController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateEventRequest $request, Event $event)
+    public function update(Request $request, Event $event)
     {
-        //
+        $event->name = $request->name;
+        $event->description = $request->description;
+        $event->date = $request->date;
+        $event->location = $request->location;
+        $event->user_id = $request->user_id;
+        $event->save();
+        return response()->json(['message' => 'Event update successfully', 'event' => $event], 201);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Event $event)
+    public function destroy($id)
     {
-        //
+        $event = Event::find($id);
+        if (!$event) {
+            return response()->json(['message' => 'Event not found'], 404);
+        }
+        $event->delete();
+        return response()->json(['message' => 'Event deleted successfully'], 200);
     }
 }
