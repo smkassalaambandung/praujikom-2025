92635389403f4001851aa3eccd5c2e052bd4134d
mas-olipiskandar <mas@olipiskandar.com>
2025-01-14 11:20:56 +0700

    Add token validation endpoint to API routes for user authentication

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\EventController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
@@ -12,4 +13,17 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('profile', [AuthController::class, 'profile']);
     Route::get('your-event', [EventController::class, 'yourEvent']);
     Route::apiResource('events', EventController::class);
+    Route::get('check-token', function (Request $request) {
+        $user = $request->user();
+        if ($user) {
+            return response()->json([
+                'message' => 'Token is valid',
+                'user' => $user,
+            ], 200);
+        } else {
+            return response()->json([
+                'error' => 'Token is invalid',
+            ], 400);
+        }
+    });
 });
